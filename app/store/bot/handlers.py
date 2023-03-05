6382753d032d6834333409add527611d84f38de3
Rusih100 <rusih100@gmail.com>
2023-03-05 13:28:43 +1000

    Added the action field in UpdateMessage

--- app/store/bot/handlers.py
+++ app/store/bot/handlers.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from enum import Enum, StrEnum
 
 from app.store.bot.router import Router
-from app.store.vk_api.dataclasses import Message, Update, Keyboard, Button
+from app.store.vk_api.dataclasses import Button, Keyboard, Message, Update
 
 if typing.TYPE_CHECKING:
     from app.web.app import Application
@@ -45,13 +45,17 @@ async def start_command(update: "Update", app: "Application"):
     # TODO: Сделать по инвайту бота в чат
     # TODO: Получить список пользователей
 
-    message_text = f"Привет! Со мной можно сыграть в BlackJAck {ServiceSymbols.LINE_BREAK}"
+    message_text = (
+        f"Привет! Со мной можно сыграть в BlackJAck {ServiceSymbols.LINE_BREAK}"
+    )
 
     message = Message(peer_id=update.object.message.peer_id, text=message_text)
     await app.store.vk_api.send_message(message=message)
 
-    message_text = f"Список команд - {BotCommands.HELP.value.command} {ServiceSymbols.LINE_BREAK}" \
-                   f"Чтобы начать игру используй команду {BotCommands.START_GAME.value.command}"
+    message_text = (
+        f"Список команд - {BotCommands.HELP.value.command} {ServiceSymbols.LINE_BREAK}"
+        f"Чтобы начать игру используй команду {BotCommands.START_GAME.value.command}"
+    )
 
     message = Message(peer_id=update.object.message.peer_id, text=message_text)
     await app.store.vk_api.send_message(message=message)
@@ -72,14 +76,14 @@ async def help_command(update: "Update", app: "Application"):
 
 # FIXME: Это заготовки для будущего кода
 
+
 @router.handler(commands=[BotCommands.START_GAME.value.command])
 async def invite_keyboard(update: "Update", app: "Application"):
     """
     Рассылает клавиатуру с опросом, будет ли пользователь играть
     """
     message = Message(
-        peer_id=update.object.message.peer_id,
-        text="Начинаем игру"
+        peer_id=update.object.message.peer_id, text="Начинаем игру"
     )
     await app.store.vk_api.send_message(message)
 
@@ -92,16 +96,15 @@ async def invite_keyboard(update: "Update", app: "Application"):
                 Button.TextButton("Да буду!", "invite_keyboard_yes"),
                 Button.TextButton("Нет", "invite_keyboard_no"),
             ]
-        ]
+        ],
     )
     message = Message(
-        peer_id=update.object.message.peer_id,
-        text="Будешь играть?"
+        peer_id=update.object.message.peer_id, text="Будешь играть?"
     )
     await app.store.vk_api.send_message(message, keyboard)
 
 
-@router.handler(buttons_payload=['invite_keyboard_yes'])
+@router.handler(buttons_payload=["invite_keyboard_yes"])
 async def invite_keyboard_yes(update: "Update", app: "Application"):
     """
     Действие, если пользователь согласился играть
@@ -109,14 +112,11 @@ async def invite_keyboard_yes(update: "Update", app: "Application"):
 
     # TODO: Логика добавления игрока
 
-    message = Message(
-        peer_id=update.object.message.peer_id,
-        text=f"Отлично"
-    )
+    message = Message(peer_id=update.object.message.peer_id, text=f"Отлично")
     await app.store.vk_api.send_message(message)
 
 
-@router.handler(buttons_payload=['invite_keyboard_no'])
+@router.handler(buttons_payload=["invite_keyboard_no"])
 async def invite_keyboard_no(update: "Update", app: "Application"):
     """
     Действие, если пользователь отказался играть
--- app/store/vk_api/dataclasses/update.py
+++ app/store/vk_api/dataclasses/update.py
@@ -1,27 +1,36 @@
 import json
 from dataclasses import dataclass
-from typing import Optional, Union
+from enum import StrEnum
+from typing import Any, Dict, Optional, Union
+
+
+class UpdateTypes(StrEnum):
+    MESSAGE_NEW = "message_new"
 
 
 @dataclass
 class Update:
     type: str
-    object: Union["ObjectMessageNew", None]
+    object: Optional["ObjectMessageNew"]
     group_id: int
 
     @classmethod
-    def parse_update(cls, raw_update) -> Union["Update", None]:
-        update_type = raw_update["type"]
-        raw_update_object = raw_update["object"]
+    def parse_update(cls, raw_update: Dict[str, Any]) -> Union["Update", None]:
+        update_type = raw_update.get("type", None)
+        update_group_id = raw_update.get("group_id", None)
+        raw_update_object = raw_update.get("object", None)
 
-        if update_type == "message_new":
+        if not all((update_type, update_group_id, raw_update_object)):
+            return None
+
+        if update_type == UpdateTypes.MESSAGE_NEW:
             update_object = ObjectMessageNew.parse_object(raw_update_object)
         else:
             return None
 
         return cls(
-            type=raw_update["type"],
-            group_id=raw_update["group_id"],
+            type=update_type,
+            group_id=update_group_id,
             object=update_object,
         )
 
@@ -34,46 +43,69 @@ class ObjectMessageNew:
     message: "UpdateMessage"
 
     @classmethod
-    def parse_object(cls, raw_update_object) -> "ObjectMessageNew":
+    def parse_object(
+        cls, raw_update_object: Dict[str, Any]
+    ) -> "ObjectMessageNew":
         message: dict = raw_update_object["message"]
-
-        raw_payload = message.get("payload", None)
-
-        if raw_payload:
-            payload_json = json.loads(raw_payload)
-            payload = Payload(
-                button=payload_json.get("button", None),
-                command=payload_json.get("command", None),
-            )
-        else:
-            payload = None
 
         return cls(
             message=UpdateMessage(
-                id=message["id"],
-                date=message["date"],
-                peer_id=message["peer_id"],
-                from_id=message["from_id"],
-                text=message["text"],
-                conversation_message_id=message["conversation_message_id"],
-                payload=payload,
+                id=message.get("id", None),
+                date=message.get("date", None),
+                peer_id=message.get("peer_id", None),
+                from_id=message.get("from_id", None),
+                text=message.get("text", None),
+                conversation_message_id=message.get(
+                    "conversation_message_id", None
+                ),
+                payload=Payload.parse_payload(message),
+                action=UpdateMessageAction.parse_action(message),
             )
         )
 
 
 @dataclass
 class UpdateMessage:
-    id: int
-    date: int
-    peer_id: int
-    from_id: int
-    text: str
-    conversation_message_id: int
-
+    id: Optional[int]
+    date: Optional[int]
+    peer_id: Optional[int]
+    from_id: Optional[int]
+    text: Optional[str]
+    conversation_message_id: Optional[int]
     payload: Optional["Payload"]
+    action: Optional["UpdateMessageAction"]
+
+
+@dataclass
+class UpdateMessageAction:
+    type: Optional[str]
+    member_id: Optional[int]
+
+    @classmethod
+    def parse_action(
+        cls, message: Dict[str, Any]
+    ) -> Union["UpdateMessageAction", None]:
+        action: Dict[str, Any] = message.get("action", None)
+        if action:
+            return UpdateMessageAction(
+                type=action.get("type", None),
+                member_id=action.get("member_id", None),
+            )
+        return None
 
 
 @dataclass
 class Payload:
     button: Optional[str]
     command: Optional[str]
+
+    @classmethod
+    def parse_payload(cls, message: Dict[str, Any]) -> Union["Payload", None]:
+        raw_payload = message.get("payload", None)
+        if raw_payload:
+            payload_json = json.loads(raw_payload)
+            return Payload(
+                button=payload_json.get("button", None),
+                command=payload_json.get("command", None),
+            )
+        return None
