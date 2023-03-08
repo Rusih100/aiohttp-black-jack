d1c1e9e6cf6cf39b64a887fac2ccc2fc2f9ab71a
Rusih100 <rusih100@gmail.com>
2023-03-09 09:08:45 +1000

    Migration

--- alembic/versions/9c5b7ec46ce1_init_database.py
+++ alembic/versions/9c5b7ec46ce1_init_database.py
@@ -1,16 +1,16 @@
 """Init database
 
-Revision ID: 0bfb5a218151
+Revision ID: 9c5b7ec46ce1
 Revises: 
-Create Date: 2023-03-09 08:29:53.280304
+Create Date: 2023-03-09 09:07:39.114166
 
 """
+from alembic import op
 import sqlalchemy as sa
 
-from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = '0bfb5a218151'
+revision = '9c5b7ec46ce1'
 down_revision = None
 branch_labels = None
 depends_on = None
