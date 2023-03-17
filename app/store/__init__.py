021963ee838d8b8013a0899e35f73fa553049af6
Rusih100 <rusih100@gmail.com>
2023-03-18 01:20:08 +1000

    Made 5 docker containers

--- app/store/__init__.py
+++ app/store/__init__.py
@@ -8,25 +8,55 @@ if typing.TYPE_CHECKING:
 
 
 class Store:
-    def __init__(self, app: "Application"):
-        from app.store.admin.accessor import AdminAccessor
-        from app.store.bot.accessor import WorkAccessor
-        from app.store.bot.manager import BotManager
-        from app.store.game.accessor import GameAccessor
-        from app.store.vk_api.accessor import VkApiAccessor
+    def __init__(self, app: "Application", service: str):
 
-        self.admins = AdminAccessor(app)
-        self.game = GameAccessor(app)
-        self.bots_manager = BotManager(app)
-        self.vk_api = VkApiAccessor(app)
-        self.worker = WorkAccessor(app)
+        match service:
+            case "admin-api":
+                from app.store.admin.accessor import AdminAccessor
+                from app.store.game.accessor import GameAccessor
+
+                self.admins = AdminAccessor(app)
+                self.game = GameAccessor(app)
+
+            case "vk-poller":
+                from app.store.vk_api.accessor import VkApiAccessor
+
+                self.vk_api = VkApiAccessor(app, is_poller=True)
+
+            case "vk-worker":
+                from app.store.bot.accessor import WorkAccessor
+                from app.store.bot.manager import BotManager
+                from app.store.game.accessor import GameAccessor
+                from app.store.vk_api.accessor import VkApiAccessor
+
+                self.game = GameAccessor(app)
+                self.bots_manager = BotManager(app)
+                self.vk_api = VkApiAccessor(app, is_poller=False)
+                self.worker = WorkAccessor(app)
 
 
-def setup_store(app: "Application"):
-    app.database = Database(app)
-    app.rabbitmq = RabbitMQ(app)
-    app.on_startup.append(app.database.connect)
-    app.on_cleanup.append(app.database.disconnect)
-    app.on_startup.append(app.rabbitmq.connect)
-    app.on_cleanup.append(app.rabbitmq.disconnect)
-    app.store = Store(app)
+def setup_store(app: "Application", service: str) -> None:
+
+    match service:
+        case "admin-api":
+            app.database = Database(app)
+
+            app.on_startup.append(app.database.connect)
+            app.on_cleanup.append(app.database.disconnect)
+
+        case "vk-poller":
+            app.rabbitmq = RabbitMQ(app)
+
+            app.on_startup.append(app.rabbitmq.connect)
+            app.on_cleanup.append(app.rabbitmq.disconnect)
+
+        case "vk-worker":
+            app.database = Database(app)
+            app.rabbitmq = RabbitMQ(app)
+
+            app.on_startup.append(app.database.connect)
+            app.on_cleanup.append(app.database.disconnect)
+            app.on_startup.append(app.rabbitmq.connect)
+            app.on_cleanup.append(app.rabbitmq.disconnect)
+
+    app.store = Store(app, service=service)
--- main.py
+++ main.py
@@ -1,15 +1,37 @@
+from argparse import ArgumentParser
 import os
 
 from aiohttp.web import run_app
 
 from app.web.app import setup_app
 
+MICROSERVISES = [
+    "admin-api",
+    "vk-poller",
+    "vk-worker"
+]
+
+
+def get_service_type() -> str:
+    parser = ArgumentParser(description="System for choosing a microservice")
+    parser.add_argument('-s', '--servise', choices=MICROSERVISES, default="admin-api")
+
+    namespase = parser.parse_args()
+
+    return namespase.servise
+
+
 if __name__ == "__main__":
+    service = get_service_type()
+
     run_app(
         port=8080,
         app=setup_app(
             config_path=os.path.join(
                 os.path.dirname(os.path.realpath(__file__)), "config.yml"
-            )
+            ),
+            service=service
         ),
     )
+
+
